fd0c34c2c6deef17076e0678da53cd59ffa67885
eitgovarthanan <govarthanan@eitworks.com>
2021-03-24 18:53:21 +0530

    login test cases

--- src/test/java/com/srd/resource/base.java
+++ src/test/java/com/srd/resource/base.java
@@ -33,6 +33,7 @@ public class base {
 		// ChromeOptions options = new ChromeOptions();
 		// options.addArguments("--disable-notifications");
 		System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
+		System.setProperty("webdriver.chrome.logfile", "/usr/bin/chromedriver.log");
 		//HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
 		//chromePrefs.put("credentials_enable_service", false);
 		// options.setExperimentalOption("prefs", chromePrefs);
@@ -42,7 +43,7 @@ public class base {
 		chromeOptions.setBinary("/usr/lib/chromium-browser/");
 		chromeOptions.setExperimentalOption("useAutomationExtension", false);
         chromeOptions.addArguments("--headless");
-        //chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
 
         driv = new ChromeDriver();
